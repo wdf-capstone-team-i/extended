20e2c65e7d58a171404a865471a072febb5a2569
Javier Montero <monterodotjavier@icloud.com>
2020-07-08 02:50:51 -0400

    feat: Navbar created, message box created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,27 +2,19 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import MessageForm from "../MessageBox/MessageForm";
-
+import Navbar from "../Navbar/Navbar";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
         <h1>Hello World</h1>
-        <MessageForm />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Message Box will go here</p>
+
+        <MessageForm />
       </header>
     </div>
   );
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  createMuiTheme,
+  ThemeProvider,
+  makeStyles,
+} from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import yellow from "@material-ui/core/colors/yellow";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: yellow[500],
+    },
+  },
+});
+
+function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
+    </div>
+  );
+}
+export default Navbar;
