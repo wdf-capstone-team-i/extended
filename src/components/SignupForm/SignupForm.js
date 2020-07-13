99ad42bbbeb21aa76e48955cf6267db043c9a1f5
Javier Montero <monterodotjavier@icloud.com>
2020-07-13 16:39:55 -0400

    Created signup form

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -1,7 +1,58 @@
 import React from "react";
+import { TextField, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const SignupForm = (props) => {
-  return <div>This is the signup form</div>;
+  return (
+    <div>
+      <form>
+        <TextField
+          id="firstName"
+          label="First Name"
+          type="text"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+        />
+        <br />
+
+        <TextField
+          id="lastName"
+          label="Last Name"
+          type="text"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+        />
+        <br />
+
+        <TextField
+          id="email"
+          label="Email Address"
+          type="text"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+        />
+        <br />
+        <TextField
+          id="password"
+          fullWidth
+          label="Password"
+          type="password"
+          variant="outlined"
+          margin="normal"
+        />
+        <br />
+        <br />
+        <Link className="messages" to="/messages">
+          <Button variant="contained" color="primary" fullWidth type="submit">
+            Signup
+          </Button>
+        </Link>
+      </form>
+    </div>
+  );
 };
 
 export default SignupForm;
