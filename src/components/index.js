9f7176426eb963115c930b3cb4cca49edb174ecd
Javier Montero <monterodotjavier@icloud.com>
2020-07-13 13:19:57 -0400

    file: to hold all components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import App from "./App/App";
+import SimpleMenu from "./Menu/Menu";
+import MessageForm from "./MessageForm/MessageForm";
+import Messages from "./Messages/Messages";
+import Navbar from "./Navbar/Navbar";
+import { default as Signup } from "./SignupForm/SignupForm";
