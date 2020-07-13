1c892c0f933b2da07f7ef353a02eb55dec824fd3
Javier Montero <monterodotjavier@icloud.com>
2020-07-13 13:19:24 -0400

    created signup form and changed messagebox to messageform

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
 import "./index.css";
 import App from "./components/App/App";
 import * as serviceWorker from "./serviceWorker";
