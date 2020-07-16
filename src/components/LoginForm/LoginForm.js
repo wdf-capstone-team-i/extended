af1f155d20ed9654af10deedb20a827f79416a14
Gilbert <gilbertvelez96@github.com>
2020-07-16 17:20:07 -0400

    Added LoginForm

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import {TextField, Button} from "@material-ui/core";
+
+const LoginForm = props => {
+
+    const {handleLogin} = props;
+    return (
+        <form onSubmit={handleLogin}>
+            <TextField
+                name="username"
+                label="username"
+                variant="outlined"
+            />
+
+            <TextField
+                id="password"
+                name="password"
+                label="password"
+                type="password"
+                variant="outlined"
+            />
+
+            <Button
+                variant="contained"
+                color="primary"
+                fullWidth
+                type="submit"
+            >
+                Log In
+            </Button>
+        </form>
+    )
+}
+
+export default LoginForm;
